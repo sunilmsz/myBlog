c9d6b9b7bb5607a7ef77308b751cac8f88e7577b
Sunil Kumar <sunilmsz>
2024-12-30 04:00:54 +0530

    blogs style updated

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,22 +1,33 @@
 import blogData from '../data/blogs'
+
 const imageStyle = {
-    marginInline:'auto',
-    objectFit:'cover',
-    maxHeight:'400px',
-    height:'auto',
-    maxWidth:'900px',
-    Width:'100%'
+    objectFit: 'cover',
+    maxHeight: '400px',
+    height: 'auto',
+    maxWidth: '900px',
+    width: '100%',
+    display: 'block',
+    margin: '0 auto',
 }
 
+const containerStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',    
+    height: 'auto',          
+    margin: '20px 0',        
+}
 
 const Blog = () => {
     const blog = blogData[1];
     return (
         <>
-        <h2> {blog.title}  </h2>
-        <p> {blog.title}</p>
-        <img style={imageStyle} src={`https://picsum.photos/id/${Math.floor(Math.random()*100)}/300/200`} />
-        <p>{blog.description}</p>
+        <h1 style={{textAlign:'center'}}> {blog.title}  </h1>
+        <p style={{textAlign:'center',fontSize:'22px'}}> {blog.intro}</p>
+        <div style={containerStyle}>
+                <img style={imageStyle} src={`https://picsum.photos/id/${Math.floor(Math.random() * 100)}/300/200`} alt="Blog" />
+         </div>
+        <p style={{marginInline:'25px',fontSize:'18px',lineHeight:'30px',textAlign:'justify'}}>{blog.description}</p>
         </>
     )
 }
